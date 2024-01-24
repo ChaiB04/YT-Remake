7e942692e2b04865b60cdc314e4ca0ae2194fae6
Emily Asparuhova <emilyasparuhova@gmail.com>
2024-01-24 11:08:21 +0100

    first commit
    test commit

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is not {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
