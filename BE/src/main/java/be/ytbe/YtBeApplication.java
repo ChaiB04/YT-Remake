74c048a351d57ecb90f2ec3611def13454f7ce21
ChaiB04 <cbahari66@gmail.com>
2024-01-24 10:37:02 +0100

    BE push
    Added backend

---
+++ BE/src/main/java/be/ytbe/YtBeApplication.java
@@ -0,0 +1,13 @@
+package be.ytbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YtBeApplication.class, args);
+    }
+
+}
---
+++ BE/src/test/java/be/ytbe/YtBeApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ytbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
