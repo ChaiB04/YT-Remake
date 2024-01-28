afc55d115227853481c19598dd2a7d18e6ac449e
Emily Asparuhova <emilyasparuhova@gmail.com>
2024-01-28 15:29:30 +0100

    added a route for the account settings

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -2,10 +2,7 @@ import './App.css'
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import UploadVideo from './pages/UploadVideo';
 import Profile from './pages/Profile';
-// function Home() {
-//   return <div>You mom</div>;
-// }
-
+import AccountSettings from './pages/AccountSettings';
 
 function App() {
   return (
@@ -13,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<UploadVideo />} />
         <Route path="/profile/:id" element={<Profile />} />
+        <Route path="/account-settings" element={<AccountSettings />} />
       </Routes>
     </Router>
   )
